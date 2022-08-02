8c9d057b9c7ab8438285e23082f2eebb8b412bc6
Lavinia <lavinialoghin7@gmail.com>
2022-08-03 00:08:10 +0300

    Added Blueprint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import "./App.css";
+import { Button, Card, Elevation } from "@blueprintjs/core";
 
-function App() {
+function Box() {
   return (
-    <div className="App">
+    <mesh>
+      <boxBufferGeometry attach="geometry"></boxBufferGeometry>
+      <meshLambertMaterial attach="material" color="red" />
+    </mesh>
+  );
+}
+function App() {
+  // @ts-ignore
+  return (
+    <div className="bp4-dark">
       <p>ThreeJS setup</p>
+      <Card interactive={true} elevation={Elevation.TWO}>
+        <h5>
+          <a>Blueprint</a>
+        </h5>
+      </Card>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <Box />
+      </Canvas>
     </div>
   );
 }
